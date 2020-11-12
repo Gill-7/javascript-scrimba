1fbd0549c72ae74be9cec8b81827d91dc4942ff8
Gillpreet <gillpreetsinghgill@gmail.com>
2020-11-11 22:12:54 -0800

    data-types

--- basic/index.js
+++ basic/index.js
@@ -1,4 +1,6 @@
-var header  = document.getElementById("header");
-header.style.color = 'red';
-header.style.fontSize = "20px";
-header.style.background = 'skyblue';
+
+
+var list = ['Larry', 'Joe', 'Mark'];
+console.log(list[2] + ' - Joined 1st');
+console.log(list[1] + ' - Joined 3rd');
+console.log(list[0] + ' - Joined 2nd');
